32890f87ad3ed2a3ea7017fe811b2244bdf7a47d
Orland0x <37511817+Orland0x@users.noreply.github.com>
2022-12-04 16:29:47 +0000

    foundry tooling

--- test/1_mul.ts
+++ test/1_mul.ts
@@ -1,4 +1,6 @@
 import { compile, acir_from_bytes } from '@noir-lang/noir_wasm';
+import { SinglePedersen } from '@noir-lang/barretenberg/dest/crypto/pedersen';
+import { BarretenbergWasm } from '@noir-lang/barretenberg/dest/wasm';
 import { setup_generic_prover_and_verifier, create_proof, verify_proof, create_proof_with_witness } from '@noir-lang/barretenberg/dest/client_proofs';
 import { packed_witness_to_witness, serialise_public_inputs, compute_witnesses } from '@noir-lang/aztec_backend';
 import path from 'path';
@@ -7,6 +9,24 @@ import { expect } from 'chai';
 import { ethers } from "hardhat";
 import { Contract, ContractFactory, utils } from 'ethers';
 
+export function pedersenLeftRight(
+    barretenberg: BarretenbergWasm, 
+    left: string, 
+    right: string): string {
+      let leftBuffer = Buffer.from(left, 'hex');
+      console.log(leftBuffer);
+      let rightBuffer = Buffer.from(right, 'hex');
+      console.log(rightBuffer);
+      let pedersen = new SinglePedersen(barretenberg);
+      let hashRes = pedersen.compress(leftBuffer, rightBuffer);
+      return hashRes.toString('hex')
+  }
+
+  const toFixedHex = (number: number, pad0x: boolean, length = 32) => {
+    let hexString = number.toString(16).padStart(length * 2, '0');
+    return (pad0x ? `0x` + hexString : hexString);
+  }
+
 describe("1_mul", function() {
 
     it("Should verify proof using abi and acir from typescript", async function() {        
@@ -14,11 +34,19 @@ describe("1_mul", function() {
         const compiled_program = compile(path.resolve(__dirname, '../circuits/src/main.nr')); 
         let acir = compiled_program.circuit;
         const abi = compiled_program.abi;
-        
+        const barretenberg = await BarretenbergWasm.new();
+        let pedersen: SinglePedersen;
+        pedersen = new SinglePedersen(barretenberg);
+
+        let a = 1234;
+        let b = 5678;
+        let hash = '0x'+pedersen.compressInputs([Buffer.from(toFixedHex(a, false), 'hex'), Buffer.from(toFixedHex(b, false), 'hex')]).toString('hex');
+        console.log(Buffer.from(toFixedHex(1, false), 'hex'))
+        console.log(hash);
         // Specify abi
-        abi.x = 3;
-        abi.y = 4;
-        abi.return = 12;
+        abi.x = a;
+        abi.y = b;
+        abi.return = hash;
 
         let [prover, verifier] = await setup_generic_prover_and_verifier(acir);
  
@@ -31,96 +59,96 @@ describe("1_mul", function() {
         expect(verified).eq(true)
     });
 
-    it("Should verify proof using acir from file and abi for typescript", async function() {
-        let acirByteArray = path_to_uint8array(path.resolve(__dirname, '../circuits/build/p.acir'));
-        let acir = acir_from_bytes(acirByteArray);
+    // it("Should verify proof using acir from file and abi for typescript", async function() {
+    //     let acirByteArray = path_to_uint8array(path.resolve(__dirname, '../circuits/build/p.acir'));
+    //     let acir = acir_from_bytes(acirByteArray);
 
-        // The abi can also be specified using hex strings, just make sure there is an even number of bytes
-        let abi = {
-            x: 3,
-            y: 4,
-            return: 12,
-        }
+    //     // The abi can also be specified using hex strings, just make sure there is an even number of bytes
+    //     let abi = {
+    //         x: 3,
+    //         y: 4,
+    //         return: 12,
+    //     }
 
-        let [prover, verifier] = await setup_generic_prover_and_verifier(acir);
+    //     let [prover, verifier] = await setup_generic_prover_and_verifier(acir);
  
-        const proof = await create_proof(prover, acir, abi);
+    //     const proof = await create_proof(prover, acir, abi);
 
-        const verified = await verify_proof(verifier, proof);
+    //     const verified = await verify_proof(verifier, proof);
       
-        console.log(verified);
+    //     console.log(verified);
 
-        expect(verified).eq(true)
-    });
+    //     expect(verified).eq(true)
+    // });
 
-    it("Should verify proof using witness arr", async function() {
-        let acirByteArray = path_to_uint8array(path.resolve(__dirname, '../circuits/build/p.acir'));
-        let acir = acir_from_bytes(acirByteArray);
+    // it("Should verify proof using witness arr", async function() {
+    //     let acirByteArray = path_to_uint8array(path.resolve(__dirname, '../circuits/build/p.acir'));
+    //     let acir = acir_from_bytes(acirByteArray);
 
-        let witnessByteArray = path_to_uint8array(path.resolve(__dirname, '../circuits/build/p.tr'));
-        const barretenberg_witness_arr = await packed_witness_to_witness(acir, witnessByteArray);
+    //     let witnessByteArray = path_to_uint8array(path.resolve(__dirname, '../circuits/build/p.tr'));
+    //     const barretenberg_witness_arr = await packed_witness_to_witness(acir, witnessByteArray);
 
-        let [prover, verifier] = await setup_generic_prover_and_verifier(acir);
+    //     let [prover, verifier] = await setup_generic_prover_and_verifier(acir);
     
-        const proof = await create_proof_with_witness(prover, barretenberg_witness_arr);
+    //     const proof = await create_proof_with_witness(prover, barretenberg_witness_arr);
     
-        const verified = await verify_proof(verifier, proof);
+    //     const verified = await verify_proof(verifier, proof);
 
-        expect(verified).eq(true)
-    });
+    //     expect(verified).eq(true)
+    // });
 
-    it("Should verify proof using compute witness", async function() {
-        let acirByteArray = path_to_uint8array(path.resolve(__dirname, '../circuits/build/p.acir'));
-        let acir = acir_from_bytes(acirByteArray);
+    // it("Should verify proof using compute witness", async function() {
+    //     let acirByteArray = path_to_uint8array(path.resolve(__dirname, '../circuits/build/p.acir'));
+    //     let acir = acir_from_bytes(acirByteArray);
 
-        let [prover, verifier] = await setup_generic_prover_and_verifier(acir);
+    //     let [prover, verifier] = await setup_generic_prover_and_verifier(acir);
  
-        let initial_js_witness = ["0x03", "0x04", "0x0c"];
-        // NOTE: breaks without even number of bytes specified, the line below does not work
-        // let initial_js_witness = ["0x3", "0x4", "0x5100"];
+    //     let initial_js_witness = ["0x03", "0x04", "0x0c"];
+    //     // NOTE: breaks without even number of bytes specified, the line below does not work
+    //     // let initial_js_witness = ["0x3", "0x4", "0x5100"];
 
-        let barretenberg_witness_arr = compute_witnesses(acir, initial_js_witness);
+    //     let barretenberg_witness_arr = compute_witnesses(acir, initial_js_witness);
 
-        const proof = await create_proof_with_witness(prover, barretenberg_witness_arr);
+    //     const proof = await create_proof_with_witness(prover, barretenberg_witness_arr);
     
-        const verified = await verify_proof(verifier, proof);
+    //     const verified = await verify_proof(verifier, proof);
 
-        expect(verified).eq(true)
-    });
+    //     expect(verified).eq(true)
+    // });
 
 });
 
-describe('1_mul using solidity verifier', function() {
-    let Verifier: ContractFactory;
-    let verifierContract: Contract;
+// describe('1_mul using solidity verifier', function() {
+//     let Verifier: ContractFactory;
+//     let verifierContract: Contract;
 
-    before(async () => {
-        Verifier = await ethers.getContractFactory("TurboVerifier");
-        verifierContract = await Verifier.deploy();
-    });
+//     before(async () => {
+//         Verifier = await ethers.getContractFactory("TurboVerifier");
+//         verifierContract = await Verifier.deploy();
+//     });
 
-    it("Should verify using proof generated by typescript wrapper", async () => {
-        const compiled_program = compile(path.resolve(__dirname, '../circuits/src/main.nr')); 
-        let acir = compiled_program.circuit;
+//     it("Should verify using proof generated by typescript wrapper", async () => {
+//         const compiled_program = compile(path.resolve(__dirname, '../circuits/src/main.nr')); 
+//         let acir = compiled_program.circuit;
 
-        let abi = {
-            x: 3,
-            y: 4,
-            return: 12,
-        }
+//         let abi = {
+//             x: 3,
+//             y: 4,
+//             return: 12,
+//         }
 
-        let [prover, verifier] = await setup_generic_prover_and_verifier(acir);
+//         let [prover, verifier] = await setup_generic_prover_and_verifier(acir);
  
-        const proof = await create_proof(prover, acir, abi);
+//         const proof = await create_proof(prover, acir, abi);
 
-        const verified = await verify_proof(verifier, proof);
-        expect(verified).eq(true)
+//         const verified = await verify_proof(verifier, proof);
+//         expect(verified).eq(true)
 
-        const sc_verified = await verifierContract.verify(proof);
-        expect(sc_verified).eq(true)
-    });
+//         const sc_verified = await verifierContract.verify(proof);
+//         expect(sc_verified).eq(true)
+//     });
 
-});
+// });
 
 function path_to_uint8array(path: string) {
     let buffer = readFileSync(path);
